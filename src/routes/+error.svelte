1dbf47594bddab0a4804dfed10c7886f90969c8f
sagnikc395 <sagnikchatterjee395@gmail.com>
2025-05-23 01:07:26 +0530

    component: added error component

---
+++ src/routes/+error.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+
+	import { Bug } from 'lucide-svelte';
+</script>
+
+<div class="layout-md">
+	<div class="">
+		<Bug class="mr-2" />
+		<span class="font-medium">{$page.status}</span>
+		<div class="mx-4 h-8 border-l border-black" />
+		{$page.error?.message}
+	</div>
+</div>
