c7936c563b85288f5c8e417599d9ef00f9dc031f
sagnikc395 <sagnikchatterjee395@gmail.com>
2025-05-22 16:05:05 +0530

    deploy

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,21 @@
 import { mdsvex } from 'mdsvex';
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const config = {
 	preprocess: [vitePreprocess(), mdsvex()],
-	kit: { adapter: adapter() },
+	kit: {
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: undefined,
+			precompress: false,
+			strict: true
+		})
+	},
+	paths: {
+		base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+	},
 	extensions: ['.svelte', '.svx']
 };
 
