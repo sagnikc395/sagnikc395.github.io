6435a084e0550a9feec16fbb31a330f5dc25df86
sagnikc395 <sagnikchatterjee395@gmail.com>
2025-05-23 01:01:35 +0530

    component: added a footer component

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,31 @@
+<footer class="layout-md mt-20 text-lg flex flex-col">
+	<div class="row">
+		<span>X</span>
+		<hr />
+		<a class="link" href="https://x.com/sagnikcw">@sagnikcw</a>
+	</div>
+	<div class="row">
+		<span>Github</span>
+		<hr />
+		<a class="link" href="https://github.com/sagnik395">@sagnik395</a>
+	</div>
+	<div class="row">
+		<span>Email</span>
+		<hr />
+		<a class="link" href="mailto:sagnikchatterjee395@gmail.com">Email</a>
+	</div>
+</footer>
+
+<style lang="postcss">
+	.row {
+		@apply flex items-center space-x-4;
+	}
+
+	.row span {
+		@apply text-neutral-500;
+	}
+
+	.row hr {
+		@apply w-full mt-0.5 border-neutral-300 border-dotted;
+	}
+</style>
