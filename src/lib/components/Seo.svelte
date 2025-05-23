197329f1dbafc78212c2bcddc7864530c37ee79e
sagnikc395 <sagnikchatterjee395@gmail.com>
2025-05-23 08:43:39 +0530

    component: added Seo component

---
+++ src/lib/components/Seo.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	export let title: string;
+	export let ogTitle: string | null = null;
+	export let description: string;
+</script>
+
+<svelte:head>
+	<title>{title}</title>
+	<meta name="description" content={description} />
+	<meta property="og:title" content={ogTitle ?? title} />
+	<meta property="og:description" content={description} />
+	<meta property="og:image" content="https://sagnikc395.github.io/assets/images/catush.jpeg" />
+	<meta name="twitter:card" content="summary_large_image" />
+</svelte:head>
