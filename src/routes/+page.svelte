e05fbfd9213fc7fc776136eeeb740c44bc2058a7
sagnikc395 <sagnikchatterjee395@gmail.com>
2025-05-23 11:07:45 +0530

    refactor home

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,47 +1,48 @@
 <script>
 	import Seo from '$lib/components/Seo.svelte';
 	import { ArrowUpRight } from 'lucide-svelte';
-	const featuredProjects = [
-		{
-			name: 'sshx',
-			href: '/projects#sshx',
-			desc: 'secure web-based, collaborative terminal'
-		},
-		{
-			name: 'Bore',
-			href: '/projects#bore',
-			desc: 'bypass your NAT in just 400 lines of code',
-			aside: '9500+ stars on GitHub'
-		},
-		{
-			name: 'Percival',
-			href: '/projects#percival',
-			desc: 'reactive notebooks for exploratory data analysis'
-		},
-		{
-			name: 'Rustpad',
-			href: '/projects#rustpad',
-			desc: 'self-hosted collaborative text editor',
-			aside: '6M+ downloads'
-		},
-		{
-			name: 'Crepe',
-			href: '/projects#crepe',
-			desc: 'compiled Datalog language in Rust',
-			aside: 'used by distributed databases & security systems'
-		},
-		{
-			name: 'FastSeg',
-			href: '/projects#fastseg',
-			desc: 'real-time semantic segmentation for self-driving cars'
-		},
-		{
-			name: 'Set with Friends',
-			href: '/projects#setwithfriends',
-			desc: 'online multiplayer card game',
-			aside: '30M+ page views, 900K+ users'
-		}
-	];
+	// let featuredProjects = [
+	// 	{
+	// 		name: 'sshx',
+	// 		href: '/projects#sshx',
+	// 		desc: 'secure web-based, collaborative terminal'
+	// 	},
+	// 	{
+	// 		name: 'Bore',
+	// 		href: '/projects#bore',
+	// 		desc: 'bypass your NAT in just 400 lines of code',
+	// 		aside: '9500+ stars on GitHub'
+	// 	},
+	// 	{
+	// 		name: 'Percival',
+	// 		href: '/projects#percival',
+	// 		desc: 'reactive notebooks for exploratory data analysis'
+	// 	},
+	// 	{
+	// 		name: 'Rustpad',
+	// 		href: '/projects#rustpad',
+	// 		desc: 'self-hosted collaborative text editor',
+	// 		aside: '6M+ downloads'
+	// 	},
+	// 	{
+	// 		name: 'Crepe',
+	// 		href: '/projects#crepe',
+	// 		desc: 'compiled Datalog language in Rust',
+	// 		aside: 'used by distributed databases & security systems'
+	// 	},
+	// 	{
+	// 		name: 'FastSeg',
+	// 		href: '/projects#fastseg',
+	// 		desc: 'real-time semantic segmentation for self-driving cars'
+	// 	},
+	// 	{
+	// 		name: 'Set with Friends',
+	// 		href: '/projects#setwithfriends',
+	// 		desc: 'online multiplayer card game',
+	// 		aside: '30M+ page views, 900K+ users'
+	// 	}
+	// ];
+	// featuredProjects.length = 0;
 </script>
 
 <Seo
@@ -49,113 +50,97 @@
 	description="Software engineer, researcher interested in program synthesis."
 />
 
-<p class="layout-md text-gray-50 text-xl leading-tight font-light mb-16" id="sagnik-is">
-	<span class="g">is a</span> software engineer<span class="g">,</span><br />
-	researcher<span class="g">, and</span><br />
-	designer<span class="g">.</span>
+<p
+	class="layout-md text-neutral-700 text-lg md:text-xl leading-tight font-light mb-16"
+	id="sagnik-is"
+>
+	<span class="neutral">is a</span>
+	software engineer<span class="neutral">,ai fanboy </span>
+	<br />
+	and researcher<span class="neutral"></span>
+	<br />
 </p>
 
-<div class="layout-md text-lg space-y-14">
-	<div class="space-y-5">
-		<p>
-			I currently work at <a class="link" href="https://modal.com">Modal</a>, a new cloud computing
-			platform.
-		</p>
+<div class="layout-md text-lg md:text-xl space-y-14 max-w-4xl mx-auto">
+	<!-- hero -->
+	<div
+		class="flex flex-col md:flex-row items-center md:items-start space-y-10 md:space-y-0 md:space-x-10"
+	>
+		<!-- Profile Image -->
+		<div class="w-full md:w-1/3">
+			<img
+				alt="sagnik chilling in his natural place"
+				src="/assets/images/profile.jpeg"
+				class="rounded-xl w-full object-cover"
+			/>
+		</div>
 
-		<p>
-			I hope to develop better <em>interactive software</em> for people to cultivate and share ideas,
-			and to meaningfully express what brings them joy.
-		</p>
-
-		<p>
-			Towards that goal, I am a generalist and care deeply about <em>systems</em>, which form the
-			fundamental building blocks of our applications; and
-			<em>interaction design</em>, how we use and live with computers that are all around us.
-		</p>
+		<!-- Text Content -->
+		<div class="w-full md:w-2/3 space-y-5">
+			<p class="text-xl font-semibold">Hi, I’m Sagnik</p>
+			<p class="text-lg md:text-xl">
+				I'm primarily interested in compilers and program synthesis, but I also make time to explore
+				reinforcement learning and energy-based models in my free time.
+			</p>
+			<p>
+				After sunset, you’ll usually find me nose-deep in a history book—anything from ancient
+				empires to modern revolutions. If I’m not reading, I’m probably out chasing pavement on a
+				long run, clearing my head one mile at a time. I’ve got a soft spot for brutal workouts and
+				quiet routines, and I genuinely believe a good sweat can fix almost anything. Weekends are
+				for recharging: wandering through random Wikipedia rabbit holes, meal-prepping like a monk,
+				or just planning my next read.
+			</p>
+		</div>
+	</div>
+	<!-- beliefs !~!! -->
+	<div class="text-center space-y-4">
+		<h2 class="text-xl md:text-2xl font-semibold underline">Some things I believe in:</h2>
+		<ul class=" list-inside space-y-1 text-neutral-500">
+			<li>Complexity is often just poor organization.</li>
+			<li>Mastery begins where imitation ends.</li>
+			<li>Clarity is speed disguised as patience.</li>
+			<li>Curiosity scales better than discipline.</li>
+			<li>Abstractions are escape hatches, not destinations.</li>
+			<li>You don’t need permission to be excellent.</li>
+			<li>The best optimizations start with deletion.</li>
+			<li>Most truths are discovered backwards.</li>
+			<li>Tools shape thought—choose wisely.</li>
+			<li>Nothing works until it works with people.</li>
+			<li>Fundamentally, every bottleneck is a skill issue.</li>
+		</ul>
 	</div>
 
-	<div class="leading-snug space-y-4">
-		<p class="pb-2">Notable open-source work:</p>
+	<!-- rn dont render the projects yet
+	{#if featuredProjects.length > 0}
+		<div class="leading-snug space-y-4">
+			<p class="pb-2">Notable open-source work:</p>
 
-		{#each featuredProjects as project}
-			<a class="project-pair" href={project.href}>
-				<div class="text-black font-medium">
-					{project.name}
-					<ArrowUpRight size={18} class="inline text-neutral-400" />
-				</div>
-				<div>
-					<p>{project.desc}</p>
-					{#if project.aside}
-						<aside>{project.aside}</aside>
-					{/if}
-				</div>
-			</a>
-		{/each}
-	</div>
-
-	<div class="space-y-5">
-		<p>
-			Previously, I worked at <a class="link" href="https://prosper.org">Prosper Robotics</a>,
-			<a class="link" href="https://convex.dev">Convex</a>,
-			<a class="link" href="https://jumptrading.com">Jump Trading</a>,
-			<a class="link" href="https://scale.com">Scale AI</a>, and
-			<a class="link" href="https://nvidia.com">Nvidia</a>. I’ve also independently published things
-			in the past, like a
-			<a
-				class="link"
-				href="https://pubs.aip.org/books/monograph/129/F-ma-Contests2011-2019-Solutions-Manual"
-				>physics book</a
-			>,
-			<a
-				class="link"
-				href="https://www.combinatorics.org/ojs/index.php/eljc/article/view/v28i1p24/pdf"
-				>combinatorics research</a
-			>, and
-			<a class="link" href="https://github.com/ekzhang/graphics-workshop">graphics tutorials</a>
-			used by thousands. Before that, I won two gold medals at the
-			<a class="link" href="https://stats.ioinformatics.org/people/6717"
-				>International Olympiad in Informatics</a
-			>.
-		</p>
-
-		<p>
-			I recently graduated from Harvard, where I was a teaching fellow for upper-level CS classes,
-			wrote my <a class="link" rel="external" href="/assets/pdf/Senior_Thesis.pdf"
-				>thesis on Datalog</a
-			>, and studied art, music, and sociology. I am a classical violist and enjoy making music with
-			others (<a class="link" href="https://www.youtube.com/live/_T4IVv0becE">1</a>,
-			<a
-				class="link"
-				href="https://www.thecrimson.com/article/2023/4/18/Natasha-pierre-great-comet-1812-musical-preview/"
-				>2</a
-			>, <a class="link" href="https://youtu.be/4ysgUA-rKUw">3</a>,
-			<a class="link" href="https://youtu.be/rYrVFTzP4sw">4</a>,
-			<a class="link" href="https://www.harvardbachsoc.org/">5</a>,
-			<a
-				class="link"
-				href="https://news.harvard.edu/gazette/story/2019/11/west-side-story-explores-racial-ethnic-political-complications/"
-				>6</a
-			>,
-			<a class="link" href="https://youtu.be/OcucfY2Kzqo">7</a>,
-			<a class="link" href="https://youngarts.org/">8</a>).
-		</p>
-
+			{#each featuredProjects as project}
+				<a class="project-pair" href={project.href}>
+					<div class="text-black font-medium">
+						{project.name}
+						<ArrowUpRight size={18} class="inline text-neutral-400" />
+					</div>
+					<div>
+						<p>{project.desc}</p>
+						{#if project.aside}
+							<aside>{project.aside}</aside>
+						{/if}
+					</div>
+				</a>
+			{/each}
+		</div>
+	{/if} -->
+	<!-- other stuff that interests me -->
+	<div class="text-center text-zinc-800">
 		<p>Other interests: math, physics, reading, photography, and biking.</p>
-	</div>
-
-	<div class="space-y-5">
-		<!-- <p>This is what I look like:</p> -->
-		<img
-			alt="Self-portrait photograph of Eric Zhang at his desk"
-			src="/assets/images/profile5.jpg"
-		/>
 	</div>
 </div>
 
 <style lang="postcss">
 	@reference "tailwindcss";
 	.g {
-		@apply text-zinc-50;
+		@apply text-neutral-900;
 	}
 	em {
 		@apply font-serif text-[110%] leading-[100%];
@@ -166,7 +151,7 @@
 	}
 
 	aside {
-		@apply mt-0.5 text-base text-zinc-50;
+		@apply mt-0.5 text-base text-slate-700;
 	}
 
 	/* Correction for vertical navigation links on mobile. */
