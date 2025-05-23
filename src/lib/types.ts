64c9cb768debc96cd58b8fed1b71261ff921879d
sagnikc395 <sagnikchatterjee395@gmail.com>
2025-05-23 09:50:55 +0530

    ohh baby

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -4,3 +4,15 @@ export type Blog = {
 	summary: string;
 	link?: string;
 };
+
+export type Project = {
+	title: string;
+	date: string;
+	content: string;
+	repo: string;
+	topics: string[];
+	lead: string;
+	image: string;
+	image_border?: boolean;
+	subimages?: string[];
+};
